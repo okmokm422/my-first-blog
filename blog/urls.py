675aa5b95fa1192b259c5756472c56d7b4fe6429
okmokm422 <okmokm422@gmail.com>
2020-10-15 23:36:07 +0900

    20201015

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 # post_list という名前の ビュー をルートURLに割り当て
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
